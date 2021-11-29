e658b0b7344200f618c8396408d2d855b08089c1
liulanhao <1712224776@163.com>
2021-11-29 20:02:24 +0800

    worker2 commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,5 +3,6 @@ public class hello {
         System.out.println("hello world");
         System.out.println("hello llh");
         System.out.println("worker");
+        System.out.println("woker2");
     }
 }
