e74540da249c114279841c6f0dacac923d4aa6b9
liulanhao <1712224776@163.com>
2021-11-29 20:03:09 +0800

    master2 commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -3,6 +3,6 @@ public class hello {
         System.out.println("hello world");
         System.out.println("hello llh");
         System.out.println("master");
-        System.out.println("worker");
+        System.out.println("worker2");
     }
 }
